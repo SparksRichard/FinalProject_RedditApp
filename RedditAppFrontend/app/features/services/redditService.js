65917b4dae60947bf973a995fa40abaea3c04c23
SparksRichard <rsparks@Zips-MacBook-Pro-47.local>
2015-11-25 16:16:30 -0500

    Minor database tweak v2.0

--- RedditAppFrontend/app/features/services/redditService.js
+++ RedditAppFrontend/app/features/services/redditService.js
@@ -2,20 +2,18 @@ angular.module('redditSearch.services',[])
 
 .factory('redditSearchService', function($http, $window){
 
-var redditUrlList = {};
+	var redditUrlList = undefined;
 
 	function sendRedditSearch(searchReddit){
-
 		$http({
-
 			method: 'GET',
 			URL: 'http://127.0.0.1.8080/search',
 			data: searchReddit
 		}).then(function successCallback(output){
-
-
-
+			redditUrlList = output.data;
+			$window.location.href='#/'
 		}) 
 	}
+	function
 
 })
