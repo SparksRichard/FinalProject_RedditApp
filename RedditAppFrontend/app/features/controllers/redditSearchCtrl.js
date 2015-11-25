f26cc4e97aceb7ca027515084dd6c44af815c63c
Marzuwq <Marzuwq.Muhammad@gmail.com>
2015-11-25 16:52:52 -0500

    Minor database tweak v2.0

--- RedditAppFrontend/app/features/controllers/redditSearchCtrl.js
+++ RedditAppFrontend/app/features/controllers/redditSearchCtrl.js
@@ -1,7 +1,18 @@
 angular.module('redditSearch.controller',[])
 
-.controller('redditSearchCtrl',function($scope, $window){
-$scope.redditUrlList = {};
+.controller('redditSearchCtrl',function($scope, $window, redditService){
+	$scope.redditUrlList = {};
+
+	$scope.sendSearch = function(){
+		var targetSubreddit = $scope.search;
+		redditService.sendRedditSearch(targetSubreddit);
+	}
+	$scope.$watch(function(){return redditService.getVerified();}, function(newVal, oldVal) {
+		if (newVal !== "undefined" ) {
+			$scope.redditUrlList = newVal;
+			$window.location.href='#/question';
+		}
+	}) 
 
 
 
