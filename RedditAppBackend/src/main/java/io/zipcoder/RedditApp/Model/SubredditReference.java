85c7d3bc6c41848003a23b651c397183ebaae23d
Marzuwq <Marzuwq.Muhammad@gmail.com>
2015-11-30 09:53:44 -0500

    Added Http request

--- RedditAppBackend/src/main/java/io/zipcoder/RedditApp/Model/SubredditReference.java
+++ RedditAppBackend/src/main/java/io/zipcoder/RedditApp/Model/SubredditReference.java
@@ -1,7 +1,55 @@
 package io.zipcoder.RedditApp.Model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Date;
+
 /**
  * Created by rsparks on 11/25/15.
  */
+@Entity
+@Table(name = "SubredditReferences")
 public class SubredditReference {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String search;
+    @NotNull
+    private Date time;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public SubredditReference(long id, String search, Date time){
+        this.id = id;
+        this.search = search;
+        this.time = time;
+    }
+    public SubredditReference(){ }
 }
