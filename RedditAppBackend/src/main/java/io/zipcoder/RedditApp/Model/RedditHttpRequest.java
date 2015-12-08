657ecda66731e61dffa3186baab04c73d3663c51
SparksRichard <sparks.richardc@gmail.com>
2015-12-07 21:09:45 -0500

    fixed issue with nullpointer when request hadn't gotten back yet (hopefully)

--- RedditAppBackend/src/main/java/io/zipcoder/RedditApp/Model/RedditHttpRequest.java
+++ RedditAppBackend/src/main/java/io/zipcoder/RedditApp/Model/RedditHttpRequest.java
@@ -35,17 +35,11 @@ public class RedditHttpRequest {
     public ArrayList<RedditLink> getRedditData(String sub, long requestNumber) {
         long currentDate= (new Date()).getTime();
         requestDate = currentDate;
-
+        response = null;
         if (!"".equals(sub)) {
             sub = "r/" + sub;
         }
-        try {
-            response = Unirest.get("https://www.reddit.com/{sub}.json")
-                    .routeParam("sub", sub)
-                    .asJson();
-        } catch (Exception e) {
-            System.out.println("Request failed");
-        }
+        response = httpRequest(sub);
         if (!response.getBody().getObject().getJSONObject("data").getJSONArray("children").isNull(0)) {
             redditResponseOf25Links = redditParser.parseData(response, requestNumber);
             return redditResponseOf25Links;
@@ -53,6 +47,16 @@ public class RedditHttpRequest {
         else {
             return null;
         }
+    }
+    public HttpResponse<JsonNode> httpRequest(String sub){
+        try {
+            response = Unirest.get("https://www.reddit.com/{sub}.json")
+                    .routeParam("sub", sub)
+                    .asJson();
+        } catch (Exception e) {
+            System.out.println("Request failed");
+        }
+        return response;
     }
 
     public RedditParser getRedditParser() {
